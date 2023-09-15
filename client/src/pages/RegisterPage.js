1d4c2f590fdebd84578aa95177b58fd88d5c9391
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-09-16 04:08:27 +0600

    add login & register

---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,9 @@
+export default function RegisterPage(){
+    return (
+        <form action="">
+            <input type="text" placeholder="username"/>
+            <input type="text" placeholder="password"/>
+            <button>Register</button>
+        </form>
+    )
+}

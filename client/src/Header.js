7bf5143e6119b80f5e2b37ae1503bcd5f8f878b6
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-09-16 04:09:35 +0600

    Update for Login & Register to function

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export default function Header(){
     return (
         <header>
-        <a className="logo">MyBlog</a>
+        <Link to="/" className="logo">MyBlog</Link>
         <nav>
-          <Link href="/login">Login</Link>
-          <Link href="">Register</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
         </nav>
       </header>
     );

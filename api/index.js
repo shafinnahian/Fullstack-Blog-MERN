f7c44e8226d69e29063a0015c56920f3241d3822
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-09-20 04:39:59 +0600

    Install bcryptjs for encryption

--- api/index.js
+++ api/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req, res) => {
+mongoose.connect('mongodb+srv://shafinnahian:kJLGNSh0Bk6D1WOm@cluster0.gw3qxyb.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async (req, res) => {
     const {username, password} = req.body;
-    res.json({requestData: {username, password}});
+    try{
+        const userDoc = await User.create({
+            username,
+            password
+        });
+        res.json(userDoc);
+    } catch(e){
+        res.status(400).json(e);
+    }
 });
 
 app.listen(4000);

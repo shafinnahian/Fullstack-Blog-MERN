9ea31f8a4b85bace925958bbc44dd689a86479a8
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-09-20 05:20:07 +0600

    Update express.js to fetch login creds (LoginPage -> api/index.js)

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
+
 
 mongoose.connect('mongodb+srv://shafinnahian:kJLGNSh0Bk6D1WOm@cluster0.gw3qxyb.mongodb.net/?retryWrites=true&w=majority');
 
@@ -14,14 +18,27 @@ app.post('/register', async (req, res) => {
     try{
         const userDoc = await User.create({
             username,
-            password
+            password: bcrypt.hashSync(password, salt),
         });
         res.json(userDoc);
     } catch(e){
+        console.log(e);
         res.status(400).json(e);
     }
 });
 
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk =  bcrypt.compareSync(password, userDoc.password);
+    res.json(passOk);
+
+    if (passOk){
+        //logged in
+    } else {
+        // not logged in
+        res.status(400).json('wrong credentials')
+    }
+});
+
 app.listen(4000);
-//kJLGNSh0Bk6D1WOm
-//mongodb+srv://shafinnahian:kJLGNSh0Bk6D1WOm@cluster0.gw3qxyb.mongodb.net/?retryWrites=true&w=majority

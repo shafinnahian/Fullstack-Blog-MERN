c6d8724db32c91db89006013481a7df995b09999
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-09-23 01:44:39 +0600

    add login-register HTML-css

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Header from './Header';
 import Layout from './Layout';
 import {Routes, Route} from "react-router-dom";
 import IndexPage from './pages/IndexPage';
@@ -11,8 +10,8 @@ function App() {
     <Routes>
       <Route path='/' element={<Layout />}> {/* this was done to dynamically add content based on the page we are in */}   
         <Route index element = {<IndexPage />} />
-        <Route path={'/login'} element = {<LoginPage />} />  {/* // Header.js receives the path & routes it to pages/LoginPage.js */}
-        <Route path={'/register'} element = {<RegisterPage />} /> {/* // Header.js receives the path & routes it to pages/RegisterPage.js */}
+        <Route path={'/login'} element = {<LoginPage />} />  {/* // Header.js receives the path '/login' & routes it to pages/LoginPage.js */}
+        <Route path={'/register'} element = {<RegisterPage />} /> {/* // Header.js receives the path '/register' & routes it to pages/RegisterPage.js */}
       </Route>
     </Routes>
   );
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,5 +1,10 @@
 export default function LoginPage(){
     return (
-        <div>login page</div>
+        <form className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+            <button>Login</button>
+        </form>
     )
 }

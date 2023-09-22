bd59c0653bc2d35ad412694be50dc04e17454849
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-09-23 05:52:28 +0600

    Authenticate login verification

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
 
 app.use(cors());
@@ -9,10 +10,14 @@ app.use(express.json());    //https://chat.openai.com/share/c30c3176-1090-4b60-9
 
 mongoose.connect('mongodb+srv://shafinnahian:tqwgPlPnpUE0G2Eu@cluster0.a5ymt04.mongodb.net/?retryWrites=true&w=majority');
 
+const salt = bcrypt.genSaltSync(10);
+
 app.post('/register', async (req, res) => {   // {post: to send information with a post request}
     const {username, password} = req.body;
     try{                    //https://chat.openai.com/share/be780ae0-3592-446c-9be0-a32781e28b12
-        const userDoc = await User.create({username, password});
+        const userDoc = await User.create({
+            username, 
+            password: bcrypt.hashSync(password, salt)});
         res.json(userDoc);
     } catch(e){
         res.status(400).json(e);
@@ -22,6 +27,18 @@ app.post('/register', async (req, res) => {   // {post: to send information with
         //res.json({requestData: {username, password}}); is sending a JSON response to the client 
         //with a specific JSON structure containing the username and password fields within a requestData object.
 });
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username}); // Checking if User (from database) has element 'username' with the correct value.
+    const passOk = bcrypt.compareSync(password, userDoc.password);    // Checking unsalted password. Press on both passwords to understand the difference 
+                                                                    //(userDoc.password is from registration)
+    if (passOk){
+        // logged in
+    } else {
+        res.status(400).json('Wrong credentials');
+    }
+})
 
 app.listen(4000);
 //tqwgPlPnpUE0G2Eu
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,10 +1,40 @@
+import { useState } from "react"
+
 export default function LoginPage(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    //  The useState hook in React is used to add state to functional components. 
+    //  When you call useState with an initial value, such as useState(''), it returns an array with two elements:
+    //  The current state value & a function to update that state value.
+
+    async function login(ev){
+        ev.preventDefault();
+        await fetch('http://localhost:4000/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'COntent-Type':'application/json'},
+        })
+    }
     return (
-        <form className="login">
+        <form className="login" onSubmit={login}>
             <h1>Login</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
+            <input type="text" 
+                    placeholder="username" 
+                    value={username}
+                    onChange={ev => setUsername(ev.target.value)}/>
+            <input type="password" 
+                    placeholder="password"
+                    value={password}
+                    onChange={ev => setPassword(ev.target.value)} />
             <button>Login</button>
         </form>
     )
 }
+/* onChange={(ev) => setUsername(ev.target.value)}: 
+                        The onChange attribute is an event handler that specifies what should happen when the input's value changes. 
+                        In this case, it's a function that takes an event object ev as its argument. When the user types in the input field, this function is called.
+                        ev.target.value: Within the onChange function, ev.target refers to the element that triggered the event (in this case, the <input> element), 
+                        and ev.target.value retrieves the current value of the input field, which is the text the user has typed.
+                        setUsername(ev.target.value): This part of the onChange function sets the username state variable to the new value entered by the user. By doing so, it updates the React component's state with the latest text input value.
+                        In summary, the onChange event handler allows the component to respond to user input in the text input field. It captures the value of the input as the user types and updates the username state variable with the new value, 
+                        ensuring that the input field's value is always synchronized with the component's state. This is known as a controlled input component in React, where the component's state drives the input field's value and behavior. */

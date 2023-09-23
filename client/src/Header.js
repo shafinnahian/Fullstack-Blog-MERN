3948d5e0192b186c3e666219c31cf14cab9ebace
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-09-23 06:37:40 +0600

    Fix initial bug, structure logged in page

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,18 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {Link} from "react-router-dom"
 
 export default function Header(){
+  const [username, setUsername] = useState(null);
   useEffect(() => {
     fetch('http://localhost:4000/profile',{
       credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUsername(userInfo.username);
+      });
     })
   }, []);
     return(
         <header>
         <Link to="/" className = "logo">MyBlog</Link>
-        <nav>        
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+        <nav>
+          {username && (
+            <>
+              <Link to="/create">Create new post</Link>
+              <a>Logout</a>
+            </>
+          )}
+          {!username && (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}    
         </nav>
       </header>
     );
